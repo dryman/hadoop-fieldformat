8725a56145287687d2f47a449031c82ddb63101c
Felix Chern <idryman@gmail.com>
2014-01-27 11:57:30 -0800

    Inherit Text in FieldWritable make me able to shrink the code for 154 lines!

--- src/main/java/org/apache/hadoop/mapreduce/lib/input/FieldRecordReader.java
+++ src/main/java/org/apache/hadoop/mapreduce/lib/input/FieldRecordReader.java
@@ -28,63 +28,35 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.Seekable;
 import org.apache.hadoop.io.FieldWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.compress.CodecPool;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionCodecFactory;
-import org.apache.hadoop.io.compress.Decompressor;
-import org.apache.hadoop.io.compress.SplitCompressionInputStream;
-import org.apache.hadoop.io.compress.SplittableCompressionCodec;
 import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.util.LineReader;
 
 /**
  * Treats keys as offset in file and value as line. 
  */
-public class FieldRecordReader extends RecordReader<LongWritable, FieldWritable> {
-  private static final Log LOG = LogFactory.getLog(LineRecordReader.class);
+public class FieldRecordReader extends LineRecordReader {
+  private static final Log LOG = LogFactory.getLog(FieldRecordReader.class);
 
-  private CompressionCodecFactory compressionCodecs = null;
-  private long start;
-  private long pos;
-  private long end;
-  private LineReader in;
-  private int maxLineLength;
-  private LongWritable key = null;
-  private Text textValue = null;
   private FieldWritable value = null;
-  private Seekable filePosition;
-  private CompressionCodec codec;
-  private Decompressor decompressor;
-  private byte[] recordDelimiterBytes = null;
   private String header = null;
 
   public FieldRecordReader() {
   }
 
   public FieldRecordReader(byte[] recordDelimiter) {
-    this.recordDelimiterBytes = recordDelimiter;
+    super(recordDelimiter);
   }
 
   public void initialize(InputSplit genericSplit,
                          TaskAttemptContext context) throws IOException {
     FileSplit split = (FileSplit) genericSplit;
     
+    super.initialize(genericSplit, context);
+    
     Configuration job = context.getConfiguration();
-    this.maxLineLength = job.getInt("mapred.linerecordreader.maxlength",
-                                    Integer.MAX_VALUE);
-    start = split.getStart();
-    end = start + split.getLength();
-    final Path file = split.getPath();
-    compressionCodecs = new CompressionCodecFactory(job);
-    codec = compressionCodecs.getCodec(file);
-
-    // find and load header file
+    Path file = split.getPath();
     FileSystem fs = file.getFileSystem(job);
     Path headerPath;
     if (fs.isFile(split.getPath())){
@@ -92,111 +64,30 @@ public class FieldRecordReader extends RecordReader<LongWritable, FieldWritable>
     } else{
       headerPath = new Path(split.getPath().toString() + "/_logs/header.tsv");
     }
-    System.out.println("FieldReocrdReader reading header path: "+ headerPath);
+    LOG.debug("FieldReocrdReader reading header path: "+ headerPath);
     FSDataInputStream headerIn = fs.open(headerPath);
     BufferedReader br = new BufferedReader(new InputStreamReader(headerIn));
     header = br.readLine();
     br.close();
-    
-    // open the file and seek to the start of the split
-    FSDataInputStream fileIn = fs.open(split.getPath());
-
-    if (isCompressedInput()) {
-      decompressor = CodecPool.getDecompressor(codec);
-      if (codec instanceof SplittableCompressionCodec) {
-        final SplitCompressionInputStream cIn =
-          ((SplittableCompressionCodec)codec).createInputStream(
-            fileIn, decompressor, start, end,
-            SplittableCompressionCodec.READ_MODE.BYBLOCK);
-        in = new LineReader(cIn, job, recordDelimiterBytes);
-        start = cIn.getAdjustedStart();
-        end = cIn.getAdjustedEnd();
-        filePosition = cIn;
-      } else {
-        in = new LineReader(codec.createInputStream(fileIn, decompressor), job,
-            recordDelimiterBytes);
-        filePosition = fileIn;
-      }
-    } else {
-      fileIn.seek(start);
-      in = new LineReader(fileIn, job, recordDelimiterBytes);
-      filePosition = fileIn;
-    }
-    // If this is not the first split, we always throw away first record
-    // because we always (except the last split) read one extra line in
-    // next() method.
-    if (start != 0) {
-      start += in.readLine(new Text(), 0, maxBytesToConsume(start));
-    }
-    this.pos = start;
-  }
-  
-  private boolean isCompressedInput() {
-    return (codec != null);
   }
 
-  private int maxBytesToConsume(long pos) {
-    return isCompressedInput()
-      ? Integer.MAX_VALUE
-      : (int) Math.min(Integer.MAX_VALUE, end - pos);
-  }
-
-  private long getFilePosition() throws IOException {
-    long retVal;
-    if (isCompressedInput() && null != filePosition) {
-      retVal = filePosition.getPos();
-    } else {
-      retVal = pos;
-    }
-    return retVal;
-  }
 
   public boolean nextKeyValue() throws IOException {
-    if (key == null) {
-      key = new LongWritable();
-    }
-    key.set(pos);
-    if (textValue == null){
-      textValue = new Text();
-    }
-    if (value == null) {
-      value = new FieldWritable(header); 
-    }
-    int newSize = 0;
-    // We always read one extra line, which lies outside the upper
-    // split limit i.e. (end - 1)
-    while (getFilePosition() <= end) {
-      newSize = in.readLine(textValue, maxLineLength,
-          Math.max(maxBytesToConsume(pos), maxLineLength));
-      if (newSize == 0) {
-        break;
+    if(super.nextKeyValue()){
+      if (value == null) {
+        value = new FieldWritable(header); 
       }
-      pos += newSize;
-      if (newSize < maxLineLength) {
-        break;
-      }
-
-      // line too long. try again
-      LOG.info("Skipped line of size " + newSize + " at pos " + 
-               (pos - newSize));
-    }
-    if (newSize == 0) {
-      key = null;
-      textValue = null;
-      value = null;      
-      return false;
-    } else {
-      //value.clear();
-      // TODO: need to be optimized
-      value.setContent(textValue);
-      //value.set(header, textValue.toString().split("\\t"));
+      value.setContent(super.getCurrentValue());
       return true;
+    } else {
+      value = null;
+      return false;
     }
   }
 
   @Override
   public LongWritable getCurrentKey() {
-    return key;
+    return super.getCurrentKey();
   }
 
   @Override
@@ -204,31 +95,4 @@ public class FieldRecordReader extends RecordReader<LongWritable, FieldWritable>
     return value;
   }
 
-  /**
-   * Get the progress within the split
-   */
-  public float getProgress() {
-    if (start == end) {
-      return 0.0f;
-    } else {
-      try { 
-        return Math.min(1.0f, (getFilePosition() - start)
-            / (float) (end - start));
-      } catch (IOException ioe) {
-        throw new RuntimeException(ioe);
-      }
-    }
-  }
-
-  public synchronized void close() throws IOException {
-    try {
-      if (in != null) {
-        in.close();
-      }
-    } finally {
-      if (decompressor != null) {
-        CodecPool.returnDecompressor(decompressor);
-      }
-    }
-  }
 }
